33aeec7fc16220df028f2520ef48d802892c4fbb
Lee JaeHyun <myleje@gmail.com>
2021-07-21 15:48:26 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -21,6 +21,18 @@ public class PlayerCtrl : MonoBehaviour
         Debug.Log("h=" + h);
         Debug.Log("v=" + v);
 
+        /*
+        정규화 벡터(Normalized Vector) , 단위벡터(Unit Vector)
+        : 벡터의 크기가 1인 벡터를 의미한다.
+
+        Vector3.forward = Vector3(0, 0, 1)
+        Vector3.up      = Vector3(0, 1, 0)
+        Vector3.right   = Vector3(1, 0, 0)
+
+        Vector3.one     = Vector3(1, 1, 1)
+        Vector3.zero    = Vector3(0, 0, 0)        
+        */
+
         transform.Translate(Vector3.forward * 0.1f * v);
         transform.Translate(Vector3.right * 0.1f * h);
         //transform.position += new Vector3(0.0f, 0.0f, 0.1f);
