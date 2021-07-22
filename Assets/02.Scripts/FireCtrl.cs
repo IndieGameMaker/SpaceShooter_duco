fbc18ff20dc0cbcda522e4489337c9394c146202
Lee JaeHyun <myleje@gmail.com>
2021-07-22 13:49:01 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -9,7 +9,10 @@ public class FireCtrl : MonoBehaviour
     // 총알을 생성할 위치정보를 저장할 변수
     public Transform firePos;
 
+    // AudioSource 컴포넌트를 추출해 저장할 변수
     private new AudioSource audio;
+    // 음원파일을 저장할 변수를 선언
+    public AudioClip fireSfx;
 
     void Start()
     {
