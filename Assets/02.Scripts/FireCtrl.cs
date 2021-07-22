763a404fa0ab97d196c2d33af86ec73f3a185989
Lee JaeHyun <myleje@gmail.com>
2021-07-22 10:23:59 +0900

    .

---
+++ Assets/02.Scripts/FireCtrl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour
+{
+    // 총알 프리팹을 저장할 변수
+    public GameObject bulletPrefab;
+    // 총알을 생성할 위치정보를 저장할 변수
+    public Transform firePos;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
