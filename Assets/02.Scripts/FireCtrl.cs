2b0ec04abc95675eb9c70b45d6c4d62ce208c964
Lee JaeHyun <myleje@gmail.com>
2021-07-22 10:34:10 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -9,15 +9,14 @@ public class FireCtrl : MonoBehaviour
     // 총알을 생성할 위치정보를 저장할 변수
     public Transform firePos;
 
-
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 총알을 생성
+            // Instantiate(생성할 프리팹, 위치, 각도)
+            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        }
     }
 }
