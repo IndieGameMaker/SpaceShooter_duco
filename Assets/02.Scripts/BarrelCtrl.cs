192ec26a586976ae9f60c94372017688579aae02
Lee JaeHyun <myleje@gmail.com>
2021-07-22 16:39:42 +0900

    .

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -16,12 +16,15 @@ public class BarrelCtrl : MonoBehaviour
     public Texture[] textures;
     // textures[0], textures[1], .... textures[99]
 
+    [SerializeField]
     private new MeshRenderer renderer;
 
     void Start()
     {
         // 텍스처 -> 머티리얼 -> 메시(Mesh)
         // MeshRenderer 컴포넌트 접근 (메시랜더러.머티리얼 = 텍스처)
+        // 자신의 하위에 있는 MeshRenderer 컴포넌트를 추출
+        renderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
     }
 
     void OnCollisionEnter(Collision coll)
